e4f6ad2a93f4bf7b32b4a51cf86955db53797b41
Christoph Dorn <christoph@christophdorn.com>
2017-09-18 22:15:27 -0700

    fix

--- examples/02-HelloWorld-Web/main.sh
+++ examples/02-HelloWorld-Web/main.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash.origin.script
 
 echo "TEST_MATCH_IGNORE>>>"
+
 depend {
     "php": "@com.github/bash-origin/bash.origin.php#1",
 	"request": "@com.github/bash-origin/bash.origin.request#s1"
 }
-echo "<<<TEST_MATCH_IGNORE"
-
 
 
 local port="8080"
@@ -18,8 +17,13 @@ sleep 1
 local rid=`uuidgen`
 CALL_request expect_status 200 "http://localhost:${port}/?rid=${rid}"
 
+echo "<<<TEST_MATCH_IGNORE"
 
-echo "OK"
 
 echo "TEST_MATCH_IGNORE>>>"
+
 CALL_php stop ${port}
+
+echo "<<<TEST_MATCH_IGNORE"
+
+echo "OK"
